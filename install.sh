6231bae4f80072c06d17a4d490d36b5095f5496f
Odirlei <odirlei@localhost.localdomain>
2018-01-31 09:22:38 -0200

    Working post install

--- install.sh
+++ install.sh
@@ -128,3 +128,30 @@ sudo dnf install powerline powerline-fonts
 # https://github.com/bhilburn/powerlevel9k
 
 
+# Visual Studio Code ----------------------------------------------------------------------------
+
+sudo dnf install code 
+
+
+# Codecs ----------------------------------------------------------------------------
+
+sudo dnf install http://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-27.noarch.rpm
+sudo rpm --import /etc/pki/rpm-gpg/RPM-GPG-KEY-rpmfusion-free-fedora-27
+
+sudo dnf install http://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-27.noarch.rpm
+sudo rpm --import /etc/pki/rpm-gpg/RPM-GPG-KEY-rpmfusion-nonfree-fedora-27
+
+sudo dnf install amrnb amrwb faad2 flac ffmpeg gpac-libs lame libfc14audiodecoder mencoder mplayer x264 x265 gstreamer-plugins-espeak gstreamer-plugins-fc gstreamer-rtsp gstreamer-plugins-good gstreamer-plugins-bad gstreamer-plugins-bad-free-extras gstreamer-plugins-bad-nonfree gstreamer-plugins-ugly gstreamer-ffmpeg gstreamer1-plugins-base gstreamer1-libav gstreamer1-plugins-bad-free-extras gstreamer1-plugins-bad-freeworld gstreamer1-plugins-base-tools gstreamer1-plugins-good-extras gstreamer1-plugins-ugly gstreamer1-plugins-bad-free gstreamer1-plugins-good
+
+# Composer ----------------------------------------------------------------------------
+
+# PHP ----------------------------------------------------------------------------
+# https://www.vivaolinux.com.br/artigo/Apache-PHP-MariaDB-e-PhpMyAdmin-no-Fedora-27
+
+# MySQL ----------------------------------------------------------------------------
+# https://www.vivaolinux.com.br/artigo/Apache-PHP-MariaDB-e-PhpMyAdmin-no-Fedora-27
+
+# PhpUnit ----------------------------------------------------------------------------
+
+# Node ----------------------------------------------------------------------------
+
--- readme.md
+++ readme.md
@@ -1,6 +1,8 @@
 
 # Post Install Fedora 27
 
+#### Under construction
+
 > git clone git@github.com:odirleiborgert/post-install-fedora.git
 
 > cd post-install-fedora
@@ -17,3 +19,14 @@
 - Mozilla Firefox
 - Google Chrome
 - Zsh (Powerline fonts, OhMyZsh)
+
+
+# Todo
+- Visual Studio Code
+- DBeaver (Database)
+- Php 7.1
+- MySql
+- NodeJS
+- Composer
+- PhpUnit
+
